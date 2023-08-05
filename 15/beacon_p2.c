ec32e7487b59235869de185b0e412280b5b203a3
Radim Mifka <radimmifka@gmail.com>
2023-08-05 16:46:21 +0200

    Add aoc 2022/15 part 2

--- 15/beacon_p2.c
+++ 15/beacon_p2.c
@@ -4,6 +4,7 @@
  **/
 
 #include <ctype.h>
+#include <iso646.h>
 #include <limits.h>
 #include <math.h>
 #include <stddef.h>
@@ -18,11 +19,14 @@
 
 #define POINTS_ARE_EQUAL(pt1, pt2) ((pt1.x == pt2.x) && (pt1.y == pt2.y))
 
+#define IN_AREA(border_point)                                                  \
+  ((border_point.x >= 0 && border_point.x <= MAX_COORDINATE) &&                \
+   (border_point.y >= 0 && border_point.y <= MAX_COORDINATE))
+
 typedef struct point {
   int x;
   int y;
   int distance;
-  struct point *beacon;
 } point;
 
 typedef struct point_arr {
@@ -31,17 +35,6 @@ typedef struct point_arr {
   point *points;
 
 } point_arr;
-
-typedef struct g_row {
-  int y;
-  int blocked;
-  point_arr available;
-} g_row;
-
-typedef struct range {
-  int from;
-  int to;
-} range;
 
 void add_point(point_arr *arr, point point) {
   if (arr->size == arr->max_size) {
@@ -60,136 +53,111 @@ void init_point_array(point_arr *arr, int length) {
   arr->max_size = length;
 }
 
-void free_point_array(point_arr *arr) {
-  for (int i = 0; i < arr->size; i++) {
-    free(arr->points[i].beacon);
-  }
-  free(arr->points);
-}
+void free_point_array(point_arr *arr) { free(arr->points); }
 
 int distance(point p1, point p2) { return abs(p1.x - p2.x) + abs(p1.y - p2.y); }
 
-int is_beacon(point p, point_arr arr) {
-  for (int i = 0; i < arr.size; i++) {
-    point beacon = *(arr.points[i].beacon);
-    if (POINTS_ARE_EQUAL(p, beacon))
-      return 1;
-  }
-  return 0;
-}
+int not_blocked(point p, point_arr sensors) {
 
-void print_point_array(point_arr arr) {
-  for (int i = 0; i < arr.size; i++) {
-    printf("%d %d\n", arr.points[i].x, arr.points[i].y);
-  }
-}
-
-int compare_ranges(const void *a, const void *b) {
-  return ((range *)a)->from - ((range *)b)->from;
-}
-
-void print_ranges(range *ranges, int ranges_size) {
-  for (int i = 0; i < ranges_size; i++) {
-    printf("%d %d\n", ranges[i].from, ranges[i].to);
-  }
-}
-
-void merge_ranges(range **ranges, int *ranges_size) {
-  qsort(*ranges, *ranges_size, sizeof(range), compare_ranges);
-
-  range merged_ranges[*ranges_size + 1];
-  int merged_count = 0;
-
-  merged_ranges[0].from = (*ranges)[0].from;
-  merged_ranges[0].to = (*ranges[0]).to;
-
-  for (int i = 1; i < *ranges_size; i++) {
-    if ((*ranges)[i].from <= merged_ranges[merged_count].to + 1) {
-      if ((*ranges)[i].to > merged_ranges[merged_count].to) {
-        merged_ranges[merged_count].to = (*ranges)[i].to;
-      }
-    } else {
-      merged_count++;
-      merged_ranges[merged_count].from = (*ranges)[i].from;
-      merged_ranges[merged_count].to = (*ranges)[i].to;
-    }
-  }
-
-  for (int i = 0; i <= merged_count; i++) {
-    (*ranges)[i] = merged_ranges[i];
-  }
-  *ranges_size = merged_count + 1;
-}
-
-void get_row(g_row *row, point_arr sensors, int y) {
-
-  int minx = INT_MAX;
-  int maxx = INT_MIN;
-
-  int sensors_on_y = 0;
-
-  row->y = y;
-  row->blocked = 0;
-
-  int max_ranges = REALLOC_AMOUNT;
-  int ranges_size = 0;
-  range *ranges = malloc(max_ranges * sizeof(range));
-
-  point p = {.y = y};
   for (int i = 0; i < sensors.size; i++) {
-    point sensor = sensors.points[i];
-    point beacon = *(sensor.beacon);
-
-    if (p.y == sensor.y)
-      sensors_on_y++;
-
-    if (p.y >= sensor.y - sensor.distance &&
-        p.y <= sensor.y + sensor.distance) {
-      for (p.x = sensor.x - sensor.distance; p.x < sensor.x + sensor.distance;
-           p.x++) {
-        if (distance(p, sensor) <= sensor.distance &&
-            !POINTS_ARE_EQUAL(p, sensor) && !POINTS_ARE_EQUAL(p, beacon)) {
-          if (p.x > maxx)
-            maxx = p.x;
-          if (p.x < minx)
-            minx = p.x;
-        }
-      }
-      if (!(minx == INT_MAX && maxx == INT_MIN)) {
-        if (ranges_size == max_ranges) {
-          max_ranges += REALLOC_AMOUNT;
-          ranges = realloc(ranges, max_ranges * sizeof(range));
-        }
-        range rng = {.from = minx, .to = maxx};
-        ranges[ranges_size] = rng;
-        ranges_size++;
-        minx = INT_MAX;
-        maxx = INT_MIN;
-      }
+    if (distance(p, sensors.points[i]) <= sensors.points[i].distance) {
+      return 0;
     }
   }
-  merge_ranges(&ranges, &ranges_size);
+  return 1;
+}
 
-  for (int i = 0; i < ranges_size; i++) {
-    row->blocked += ranges[i].to - ranges[i].from + 1;
+long long int calculate_tuning_frequency(point p) {
+  return (long long int)p.x * (long long int)MAX_COORDINATE + p.y;
+}
 
-    if (i != ranges_size - 1 && ranges[i + 1].from - ranges[i + 1].to != 1) {
-      for (int j = ranges[i].to + 1; j < ranges[i + 1].from; j++) {
-        point tmp = {.x = j, .y = y};
-        if (!is_beacon(tmp, sensors))
-          add_point(&row->available, tmp);
+int traverse_border(point top, point bottom, point left, point right,
+                    point_arr sensors) {
+
+  point current = top;
+  point border_point = {.x = 0, .y = 0};
+  while (!POINTS_ARE_EQUAL(current, left)) {
+    if (POINTS_ARE_EQUAL(current, top)) {
+      border_point.x = current.x + 1;
+      border_point.y = current.y;
+      if (IN_AREA(border_point) && not_blocked(border_point, sensors)) {
+        printf("Tuning frequency: %lld\n",
+               calculate_tuning_frequency(border_point));
+        return 1;
       }
     }
-  }
-  row->blocked -= sensors_on_y;
 
-  free(ranges);
+    border_point.x = current.x - 1;
+    border_point.y = current.y;
+    if (IN_AREA(border_point) && not_blocked(border_point, sensors)) {
+      printf("Tuning frequency: %lld\n",
+             calculate_tuning_frequency(border_point));
+
+      return 1;
+    }
+
+    current.x--;
+    current.y++;
+  }
+
+  while (!POINTS_ARE_EQUAL(current, bottom)) {
+    border_point.x = current.x - 1;
+    border_point.y = current.y;
+    if (IN_AREA(border_point) && not_blocked(border_point, sensors)) {
+      printf("Tuning frequency: %lld\n",
+             calculate_tuning_frequency(border_point));
+
+      return 1;
+    }
+    current.x++;
+    current.y++;
+  }
+
+  while (!POINTS_ARE_EQUAL(current, right)) {
+    if (POINTS_ARE_EQUAL(current, bottom)) {
+      border_point.x = current.x + 1;
+      border_point.y = current.y;
+      if (IN_AREA(border_point) && not_blocked(border_point, sensors)) {
+        printf("Tuning frequency: %lld\n",
+               calculate_tuning_frequency(border_point));
+
+        return 1;
+      }
+    }
+
+    border_point.x = current.x - 1;
+    border_point.y = current.y;
+    if (IN_AREA(border_point) && not_blocked(border_point, sensors)) {
+      printf("Tuning frequency: %lld\n",
+             calculate_tuning_frequency(border_point));
+
+      return 1;
+    }
+    current.x++;
+    current.y--;
+  }
+
+  while (!POINTS_ARE_EQUAL(current, top)) {
+    border_point.x = current.x - 1;
+    border_point.y = current.y;
+    if (IN_AREA(border_point) && not_blocked(border_point, sensors)) {
+      printf("Tuning frequency: %lld\n",
+             calculate_tuning_frequency(border_point));
+
+      return 1;
+    }
+    current.x--;
+    current.y--;
+  }
+
+  return 0;
 }
 
 int main() {
   char row[MAX_LENGTH + 2];
 
   point sensor;
+  point beacon;
 
   point_arr sensors;
   init_point_array(&sensors, REALLOC_AMOUNT);
@@ -199,29 +167,30 @@ int main() {
     if (strlen(row) == 0)
       continue;
 
-    point *beacon = (point *)malloc(sizeof(point));
-    if (beacon == NULL) {
-      return 1;
-    }
-
     if (sscanf(row, "Sensor at x=%d, y=%d: closest beacon is at x=%d, y=%d",
-               &sensor.x, &sensor.y, &beacon->x, &beacon->y) == 0) {
+               &sensor.x, &sensor.y, &beacon.x, &beacon.y) == 0) {
       return 1;
     }
 
-    sensor.distance = distance(sensor, *beacon);
-    sensor.beacon = beacon;
+    sensor.distance = distance(sensor, beacon);
 
     add_point(&sensors, sensor);
   }
 
-  g_row r;
-  init_point_array(&r.available, REALLOC_AMOUNT);
-  get_row(&r, sensors, 2000000);
+  for (int i = 0; i < sensors.size; i++) {
 
-  printf("Blocked points: %d\n", r.blocked);
+    int x = sensors.points[i].x;
+    int y = sensors.points[i].y;
+    int distance = sensors.points[i].distance;
 
-  free(r.available.points);
+    point top = {.x = x, .y = y - distance};
+    point bottom = {.x = x, .y = y + distance};
+    point left = {.x = x - distance, .y = y};
+    point right = {.x = x + distance, .y = y};
+    if (traverse_border(top, bottom, left, right, sensors)) {
+      break;
+    }
+  }
   free_point_array(&sensors);
   return 0;
 }
